0ecb4a01a7367fbad7cc455d988767cafb2a4525
andricasaputra <dric.dev@gmail.com>
2022-04-04 05:57:59 +0700

    first commit

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Intro from '@/components/Intro.vue'
+import Companies from '@/components/Companies.vue'
+import Services from '@/components/Services.vue'
+import Testimonials from '@/components/Testimonials.vue'
+import Faq from '@/components/Faq.vue'
+import Porto from '@/components/Porto.vue'
+import Contact from '@/components/Contact.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<template>
+  <main>
+    <Navbar />
+    <Intro />
+    <Companies />
+    <Services />
+    <Testimonials />
+    <Faq />
+    <Porto />
+    <Contact />
+    <Footer />
+  </main>
+</template>
